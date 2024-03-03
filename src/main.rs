9b3d4a27b56369a640b82681b169f59112b66eaa
robert <robertwinstonwidjaja1@gmail.com>
2024-03-02 22:14:06 -0800

    google search functionality

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use]
+extern crate rocket;
+
+use rocket::response::Redirect;
+
+mod utils;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[get("/search?<cmd>")]
+fn search(cmd: String) -> Redirect {
+    let command = utils::get_command_from_query_string(&cmd);
+    let redirect_url = match command {
+        "tw" => String::from("https://twitter.com"),
+        _ => String::from("https:://google.com"),
+    };
+
+    Redirect::to(redirect_url)
+}
+
+fn main() {
+    rocket::ignite().mount("/", routes![index, search]).launch();
+}
